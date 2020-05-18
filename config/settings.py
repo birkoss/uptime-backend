dc825b689eaa6877bdde7c32e82cf35554cf1ae1
Mathieu Robichaud <mathieu@standish.ca>
2020-05-18 09:20:23 -0400

    Added API app and drf

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+    'rest_framework.authtoken',
+
     'user',
+    'api',
 ]
 
 MIDDLEWARE = [
--- user/models.py
+++ user/models.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 from .managers import UserManager
 
+# Custom User model
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(
         verbose_name='email address',
@@ -20,3 +24,9 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+# Create a token when an user is created
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
