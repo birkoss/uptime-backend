7e280b956748604d4626f11e738a8dcabea0085b
Mathieu Robichaud <mathieu@standish.ca>
2020-05-18 12:40:03 -0400

    Endpoint and Ping models

--- uptime/admin.py
+++ uptime/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Server, ServerProtocol
+from .models import Ping, Endpoint, Server, ServerProtocol
 
+admin.site.register(Ping)
+admin.site.register(Endpoint)
 admin.site.register(Server)
 admin.site.register(ServerProtocol)
--- uptime/models.py
+++ uptime/models.py
@@ -24,3 +24,24 @@ class Server(models.Model):
     def __str__(self):
         return self.protocol.slug + "://" + self.hostname
 
+
+class Endpoint(models.Model):
+    server = models.ForeignKey(Server, on_delete=models.PROTECT)
+    url = models.CharField(max_length=100, default='')
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.server.__str__() + self.url
+
+
+class Ping(models.Model):
+    endpoint = models.ForeignKey(Endpoint, on_delete=models.PROTECT)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    response_code = models.SmallIntegerField(db_index=True)
+    response_time = models.DecimalField(max_digits=10, decimal_places=6)    # Ex: 1234.123456
+    response_headers = models.TextField()
+    response_body = models.TextField()
+
+    class Meta:
+        ordering = ('-date_added', )
